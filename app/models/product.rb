d4e1419aeda74c4abab6cf053a047e1fa3891eda
Cat <Catalin.Mititiuc@gmail.com>
2018-05-02 08:24:53 -0400

    Add more specs

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
-  has_many :variants, :dependent => :destroy
+  has_many :variants, dependent: :destroy
 
   def self.search(term)
+    term ||= ''
     query = "MATCH (product_name) AGAINST (?) OR product_name LIKE ?"
     results = where(query, term, "%#{term.split(' ').join('%')}%")
     ActiveRecord::Associations::Preloader.new.preload(results, [:variants])
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,14 +2,50 @@ require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
   describe "GET /products/search" do
-    it "returns correct product" do
-      get products_search_path(q: 'washed chimps')
-
-      returned_product_name =
-        JSON.parse(response.body)['data']['attributes']['product_name']
+    it 'returns ok response' do
+      get products_search_path
 
       expect(response).to have_http_status(200)
-      expect(returned_product_name).to eq('washed chinos')
     end
+
+    it 'returns all products when search term is empty' do
+      get products_search_path(q: '')
+
+      expect(get_product_names(response.body)).to match_array(
+        ['washed chinos', 'jetsetter jeans', 'travel jeans', 'fireside flannels']
+      )
+    end
+
+    it "returns correct products when only 1 word matches" do
+      get products_search_path(q: 'washed chimps')
+
+      expect(get_product_names(response.body)).to match_array(['washed chinos'])
+    end
+
+    it "returns correct products when multiple records match" do
+      get products_search_path(q: 'jeans')
+
+      expect(get_product_names(response.body)).to match_array(
+        ['jetsetter jeans', 'travel jeans']
+      )
+    end
+
+    it "returns correct products with partial search term" do
+      get products_search_path(q: 'wash')
+
+      expect(get_product_names(response.body)).to match_array(['washed chinos'])
+    end
+
+    it "returns nothing when no matches" do
+      get products_search_path(q: 'blah')
+
+      expect(get_product_names(response.body)).to eq([])
+    end
+  end
+end
+
+def get_product_names(response)
+  JSON.parse(response)['data'].map do |result|
+    result['attributes']['product_name']
   end
 end
